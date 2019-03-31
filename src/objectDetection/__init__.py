15b727a6aa9de12104e594771498ed359c57eedf
edalab <edalab@edalabde-MacBook-Pro.local>
2019-03-31 10:01:06 +0800

    0.0.1

--- src/objectDetection/__init__.py
+++ src/objectDetection/__init__.py
@@ -1,14 +1,11 @@
-
 from misc import *
-
+import os
 
 class ObjectDetectionMgr:
     def __init__(self):
         pass
     def setGraph(self, image, graph):
-        pass
-
-    
-
-
+        os.system("scp -P 50035 image.jpg pwhuang@eda.ee.ntu.edu.tw:/home/pwhuang/")
+        os.system("ssh pwhuang@eda.ee.ntu.edu.tw -p 50035 'bash -s' < exe.sh")
+        os.system("scp -P 50035 pwhuang@eda.ee.ntu.edu.tw:pos.npy .")
 
