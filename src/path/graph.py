43ececf2a102dad1bfcc0437b800558a3094ae28
kevin <onedayatatime0923@gmail.com>
2019-03-31 10:45:02 +0800

    kevin

--- src/path/graph.py
+++ src/path/graph.py
@@ -130,9 +130,10 @@ class Graph:
 
     def updateEdge(self, data):
         point = np.zeros((640, 320))
-        #  print(point.shape)
+        print(point.shape)
+        print(data)
         point[data] = 1
-        #  print(point.shape)
+        print(point.shape)
         for e in self._edgeL:
             e.setWeight(np.sum(point[e.xRange()[0]:e.xRange()[1], e.yRange()[0]: e.yRange()[1]]))
     def simpleInit(self):
