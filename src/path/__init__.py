dcfb252dfa33ac8e052384ab9f4311879e8656ba
kevin <onedayatatime0923@gmail.com>
2019-03-31 10:08:48 +0800

    kevin

--- src/path/__init__.py
+++ src/path/__init__.py
@@ -15,6 +15,14 @@ class PathMgr:
     def solve(self):
         #  while True:
         image = self._rpi.query()
-        self._objectDetectionMgr.setGraph(image, self._graph)
+        data = self._objectDetectionMgr.setGraph(image)
+        setGraph(data);
         self._graph.solve()
         self._stm.configure(self._graph)
+    def setGraph(self, image, data):
+        point = np.zeros(640, 320)
+        point[data] = 1
+        for e in self._graph.edgeL():
+            e.setWeight(np.sum(point[e.xRange()[0]:e.xRange()[1], e.yRange()[0]: e.yRange()[1]]))
+
+
