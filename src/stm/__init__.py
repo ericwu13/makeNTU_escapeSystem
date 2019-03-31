bbd5f953a64813630a92742aa30c3c58441c3adb
kevin <onedayatatime0923@gmail.com>
2019-03-31 09:15:03 +0800

    kevin

--- src/stm/__init__.py
+++ src/stm/__init__.py
@@ -8,7 +8,7 @@ class Stm:
     def __init__(self):
         self._serial=serial.Serial("/dev/stdout", 9600, timeout= 0.5 )
     def configure(self, graph):
-        #  self._serial.write("s".encode())
+        self._serial.write("s".encode())
         for i in range(11):
             v = graph.vertexL()[i]
             if (v.isLight()):
